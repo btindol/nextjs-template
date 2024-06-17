4276c44527a41dc67fa0ae8c96391816b9d30f1d
btindol <blake.tindol@stryker.com>
2024-06-17 12:44:01 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// app/pages.tsx  
+export default function Home() {  
+  return (  
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">  
+      <h1 className="text-4xl font-bold">Hello, World from Next.js!</h1>  
+    </main>  
+  );  
+}  
