c5a9ff929f516793958f332cf64f3339909a98ce
btindol <blake.tindol@stryker.com>
2024-06-26 15:49:32 -0400

    auth 21

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,24 @@
-// next.config.mjs
+// // next.config.mjs
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: 'standalone',
+//   env: {
+//     AZURE_AD_CLIENT_ID: process.env.AZURE_AD_CLIENT_ID,
+//     AZURE_AD_CLIENT_SECRET: process.env.AZURE_AD_CLIENT_SECRET,
+//     AZURE_AD_TENANT_ID: process.env.AZURE_AD_TENANT_ID,
+//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+//     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+//   },
+// };
+
+// export default nextConfig;
+
+
+//////////deployed to azure
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'standalone',
   env: {
     AZURE_AD_CLIENT_ID: process.env.AZURE_AD_CLIENT_ID,
@@ -10,6 +27,10 @@ const nextConfig = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
+  experimental: {
+    outputStandalone: true,
+  },
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
