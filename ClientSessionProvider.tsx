f62a10305098df8258e26e3b1f7c08a7685fbed5
btindol <blake.tindol@stryker.com>
2024-06-27 16:21:00 -0400

    remove in app auth button flow but keep in front of app p2

--- ClientSessionProvider.tsx
+++
@@ -1,9 +0,0 @@
-// ClientSessionProvider.tsx
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export default function ClientSessionProvider({ children }: { children: React.ReactNode }) {
-  console.log("ClientSessionProvider: rendering SessionProvider");
-  return <SessionProvider>{children}</SessionProvider>;
-}
